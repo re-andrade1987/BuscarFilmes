a8daa5bdfdbdc238743eee78af178e18c63ce3dd
Renata Andrade <renataandrade1987@hotmail.com>
2018-08-19 19:04:21 -0300

    criando uma locadodora integrando api

---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.getElementById("form").addEventListener("submit", pesquisarFilmes);
+
+function pesquisarFilmes(e){
+    e.preventDefault();
+    var filmePesquisa = document.getElementById("pesquisar").value;
+    buscarFilmes(filmePesquisa);
+    
+}
+function buscarFilmes(filmePesquisa){
+    axios.get("http://www.omdbapi.com/?s=" + filmePesquisa)
+  .then(function (response) {
+      console.log(filmes)
+    var filmes = response.data.Search;
+    var mostrarFilmes = "";
+
+    for(var i = 0; i < filmes.length; i++){
+
+        mostrarFilmes +~ `
+        <div class"col-sm-6 col-md-4">
+            <div class"thumbnail">
+                <img src"${filmes[i].Poster}" class="img-thumbnail">
+                <h4>${filmes[i].Title}</h4>
+                <p><a href="#" class="btn btn-primary" role="button">Ver Detalhes</a></p>
+            </div>
+         </div>
+
+        `;
+
+    }
+    document.getElementById("movie").innerHTML = mostrarFilmes;
+})      
+
+.catch(function(error){
+    console.log(error);
+
+});
+}
